055e29c3d29d4e6e126ecb9a686600d3a173ad34
Marufjon94f <faith94nazarov@yandex.ru>
2021-07-04 21:24:33 +0500

    add changes

---
+++ main.js
@@ -0,0 +1,30 @@
+
+alert('Welcome to useless questionaire!');
+
+
+var firstName = prompt ('write your name please!:' , 'John');
+var secondName = prompt ('write your second name :', 'Edward');
+var middleName = prompt ('write your middle name :', 'Doe');
+var age = prompt ('write your age :', '60');
+var hobby = prompt ('write your hobby :', 'sleeping');
+var dislike = prompt ('write your dislike :', 'reading');
+var phoneNumber = prompt ('write your phone number :', '+99890 9000009');
+var adress = prompt ('write your adress :', 'Xadra, milliy taomlar');
+var postCode = prompt ('write your post code :', '100000');
+var review = prompt ('write your review :', 'perfect');
+
+
+console.log (
+    "name: " + firstName +
+"\nsecondName: "+secondName+
+"\nmiddleName: " + middleName+
+"\nage: " + age+
+"\nhobby: " + hobby+
+"\ndislike: " + dislike+
+"\nphoneNumber: " + phoneNumber+
+"\nadress: " + adress+
+"\npostCode: " + postCode+
+"\nreview: " + review
+
+
+)
